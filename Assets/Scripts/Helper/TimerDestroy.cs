167d57efbe83cf3ce11dafe17b29fa87707b488c
matt5797 <matt5796@naver.com>
2022-07-29 09:51:34 +0900

    Arrangement Scripts

---
+++ Assets/Scripts/Helper/TimerDestroy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WOW.Helper
+{
+    public class TimerDestroy : MonoBehaviour
+    {
+        public float destoryTime = 1.5f;
+        float currentTime = 0;
+
+        private void Start()
+        {
+            Destroy(this, destoryTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile/HighExplosive.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WOW.Projectile
+{
+    public class HighExplosive : MonoBehaviour
+    {
+        public GameObject HE_Effect;
+        private void OnCollisionEnter(Collision collision)
+        {
+            GameObject effect = Instantiate(HE_Effect);
+            effect.transform.position = transform.position;
+            Destroy(gameObject);
+        }
+    }
+}
